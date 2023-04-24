e86773b406269d0f6afe78897313480e816b0098
QuocBinh042 <tranlequocbinh2003@gmail.com>
2023-04-25 00:16:21 +0700

    Update3-Check Ma benh nha

---
+++ DemoThiCK/js/app.js
@@ -0,0 +1,9 @@
+function app() {
+    let maBN = document.getElementById('maBenhNhan').value
+    let resultMaBN = maBN.match(/(BN-)\d{5}/g);
+    if (!resultMaBN) {
+        alert('Mã bệnh nhân không hợp lệ');
+        return;
+    }
+
+}
